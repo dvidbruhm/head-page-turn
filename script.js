23dc0c9a95592048a316d25cb62019a6c279b1b4
David <david.abraham.1337@gmail.com>
2022-02-06 21:04:38 -0500

    First test using Blazeface

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
-const video = document.querySelector('#myVidPlayer');
+const video = document.querySelector("#webcam");
 
 //w-width,h-height
 var w, h;
 canvas.style.display = "none";
 
-//new
-function snapshot(){
+async function snapshot() {
+    const model = await blazeface.load();
+    w = video.clientWidth;
+    h = video.clientHeight;
+    canvas.width = w;
+    canvas.height = h;
+
     context.fillRect(0, 0, w, h);
     context.drawImage(video, 0, 0, w, h);
     canvas.style.display = "block";
+
+    console.debug(model);
+    const predictions = await model.estimateFaces(video, false);
+    console.debug("allo");
+    console.debug(predictions);
+
+    for (let i = 0; i < predictions.length; i++) {
+        const start = predictions[i].topLeft;
+        const end = predictions[i].bottomRight;
+        const size = [end[0] - start[0], end[1] - start[1]];
+
+        // Render a rectangle over each detected face.
+        context.fillRect(start[0], start[1], size[0], size[1]);
+    }
 }
 
-window.navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-    .then(stream => {
+window.navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((stream) => {
         video.srcObject = stream;
         video.onloadedmetadata = (e) => {
             video.play();
 
-            //new
-            w = video.videoWidth;
-            h = video.videoHeight
-
+            w = video.clientWidth;
+            h = video.clientHeight;
             canvas.width = w;
             canvas.height = h;
         };
     })
-    .catch(error => {
-        alert('You have to enable the mike and the camera');
+    .catch((error) => {
+        alert("You have to enable the mike and the camera");
     });
